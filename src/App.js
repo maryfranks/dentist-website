c7054e4036be38939abf7bf9b0f6de886e10ca0a
Mary Franks <maryfranks@gmail.com>
2018-01-13 14:34:42 -0500

    update homepage for dentist webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello!</h1>
+        <h1>Dentist Website</h1>
+        <p>Welcome to my denstist website</p>
       </div>
     );
   }
