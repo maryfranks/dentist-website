64623a3ea06c534a57193a0eadf81d99fce8bbaa
Mary Franks <maryfranks@gmail.com>
2018-01-13 14:43:27 -0500

    move dentist website welcome from App.js to Home.js and import Home to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Dentist Website</h1>
-        <p>Welcome to my denstist website</p>
+        <Home></Home>
       </div>
     );
   }
