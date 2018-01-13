e35468a91e5722215c189cc58eb5ec81dac3ccf2
Mary Franks <maryfranks@gmail.com>
2018-01-13 14:56:03 -0500

    add contact and procedures to app

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <p>My offices are cool</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/Procedures.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Procedures extends Component {
+  render() {
+    return (
+      <div>
+        <p>Full Service Dentistry</p>
+      </div>
+    );
+  }
+}
+
+export default Procedures;
