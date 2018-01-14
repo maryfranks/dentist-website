779310a1e894f0a4bd2c11b7df00dcb6c0e9d1f1
Mary Franks <maryfranks@gmail.com>
2018-01-14 09:38:22 -0500

    add routes and links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact'
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home></Home>
-        <Procedures></Procedures>
-        <Contact></Contact>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/procedures">Procedures</Link>{' '}
+            <Link to="/contact">Contact</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
